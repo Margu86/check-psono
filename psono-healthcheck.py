8220c44f8a233093f0acb96adc7688a517a82e02
Margu86 <benjamin.zeussel@technetex.com>
2021-10-03 17:21:15 +0200

    initial commit

---
+++ psono-healthcheck.py
@@ -0,0 +1,28 @@
+#!/usr/python3
+
+import urllib.request, os, json
+
+try:
+    healthcheck = urllib.request.urlopen("https://my.url.tld")
+    status = healthcheck.read().decode('utf-8')
+    if(str(healthcheck.getcode() == '200'):
+        data = json.loads(url.read().decode())
+        healthy = true
+        badcount = 0
+        badsrvc = ''
+        for key, value in data.items():
+            if(value != 'true'):
+                healthy = false
+                badcount += 1
+                badsrvc = key
+        if(badcount > 0):
+            if(badcount > 1):
+                print('2 "psono-health" - More than one check returned false')
+            else:
+                printstr = '1 "psono-health" - {} returned false'.format(badsrvc)
+                print(printstr)
+
+    else:
+        print('2 "psono-health" - Returncode != 200')
+except:
+    print('3 "psono-health" - An error ocurred while checking the health')
