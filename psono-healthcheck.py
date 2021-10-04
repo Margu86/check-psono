73870cf4cc63912ad7198ffe557a73760b5ecb1b
Margu86 <benjamin.zeussel@technetex.com>
2021-10-04 19:12:13 +0200

    rewritten to return every single failed check in the string

--- psono-healthcheck.py
+++ psono-healthcheck.py
@@ -8,21 +8,34 @@ try:
     status = healthcheck.read().decode('utf-8')
     if(str(healthcheck.getcode()) == '200'):
         data = json.loads(status)
-        healthy = true
         badcount = 0
         badsrvc = ''
-        for key, value in data.items():
-            if(value != 'true'):
-                healthy = false
-                badcount += 1
-                badsrvc = key
+        if not data["db_read"]["healthy"] :
+            print("db_read not healthy")
+            badsrvc += "db_read"
+            badcount += 1
+        if not data["db_sync"]["healthy"] :
+            print("db_sync not healthy")
+            if(badcount > 0):
+                badsrvc += ", db_sync"
+            else:
+                badsrvc += "db_sync"
+            badcount += 1
+        if not data["time_sync"]["healthy"] :
+            print("time_sync not healthy")
+            if(badcount > 0):
+                badsrvc += ", time_sync"
+            else:
+                badsrvc += "time_sync"
+            badcount += 1
         if(badcount > 0):
             if(badcount > 1):
-                print('2 "psono-health" - More than one check returned false')
+                printstr = '2 "psono-health" - {} returned false'.format(badsrvc)
             else:
                 printstr = '1 "psono-health" - {} returned false'.format(badsrvc)
-                print(printstr)
-
+            print(printstr)
+        else:
+            print('0 "psono-health" - all checks passed')
     else:
         print('2 "psono-health" - Returncode != 200')
 except:
